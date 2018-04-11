d0a6048f1baa5d1282527849b32c4b921efaca95
biancos <35995026+biancos@users.noreply.github.com>
2018-04-11 08:41:16 +0200

    trials Data Def

--- DataDef.R
+++ DataDef.R
@@ -1,11 +1,32 @@
 library(ggplot2)
 library(dplyr)
 library(stats)
+library(gmodels)
 
+#install.packages("gmodels")
 
-samples<-read.csv("M:/MEDIZIN/STCS/00_CDM/Queries/Samples_management/samplequalityApp/samplesQA/samplesbyorgan_22-03-2018.csv")
-projout<-read.csv("M:/MEDIZIN/STCS/00_CDM/Queries/Samples_management/samplequalityApp/samplesQA/proj_out_22-03-2018.csv")
+samples<-read.csv("./data/samplesbyorgan_22-03-2018.csv")
+projout<-read.csv("./data/proj_out_22-03-2018.csv")
 
 
 table(projout$assperiod,projout$sin,projout$soutproj)
 xtabs(~assperiod+sin,projout)
+
+CrossTable(projout$assperiod,projout$sin, prop.chisq = F, dnn=c("Assessment Period","Type of sample"))
+
+length(unique(projout$sidloc))
+
+a<-projout[duplicated(projout[,"sid"]),]
+aa <- samples%>%
+  select(patid,soasid,sid,sinali,sinst) %>%
+   arrange(sid,patid)
+
+
+a<-projout[duplicated(aa[,"sid"]),]
+
+
+d3 <- samples%>% 
+  group_by(assperiod,sin) %>% 
+  summarise(sinstock=sum(sinst), sinharvested=sum(sinali) ) %>% 
+  mutate(perc=count/sum(sinharvested)*100)
+
